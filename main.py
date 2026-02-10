79db4d6fbec0a1a1fb1794aca9df7b5a65667883
Samar Rai <rsamar918@gmail.com>
2026-02-10 08:51:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Calculator 
+import addition as a 
+import subtraction as s
+import mul as m
+
+print("addition result=",a.add(5,6))
+print("subtraction result=",s.sub(6,5))
+print("mul result=",m.mul(5,6))
+
+
+
+
